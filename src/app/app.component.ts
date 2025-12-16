447c4a959e62cc8c14003145a815efd1d446f0b7
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-12-16 16:20:22 +0530

    Added Employee List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { EmployeeListComponent } from "./pages/employee-list/employee-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EmployeeApp';
+  title = 'EmployeeManagement';
 }
--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../../models/employee.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
-  templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  imports: [CommonModule, RouterLink],
+  templateUrl: './employee-list.component.html'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employeeService = inject(EmployeeService);
+  employees: Employee[] = [
+  {
+    _id: "emp001",
+    name: "Aarav Sharma",
+    email: "aarav.sharma@company.com",
+    position: "Software Engineer",
+    department: "Engineering",
+    salary: 85000,
+  },
+  {
+    _id: "emp002",
+    name: "Neha Verma",
+    email: "neha.verma@company.com",
+    position: "UI/UX Designer",
+    department: "Design",
+    salary: 72000,
+  },
+  {
+    _id: "emp003",
+    name: "Rohit Mehta",
+    email: "rohit.mehta@company.com",
+    position: "Product Manager",
+    department: "Product",
+    salary: 98000,
+  },
+  {
+    _id: "emp004",
+    name: "Sneha Iyer",
+    email: "sneha.iyer@company.com",
+    position: "HR Executive",
+    department: "Human Resources",
+    salary: 60000,
+  },
+  {
+    _id: "emp005",
+    name: "Karan Patel",
+    email: "karan.patel@company.com",
+    position: "QA Engineer",
+    department: "Quality Assurance",
+    salary: 68000,
+  },
+  {
+    _id: "emp006",
+    name: "Pooja Nair",
+    email: "pooja.nair@company.com",
+    position: "Data Analyst",
+    department: "Analytics",
+    salary: 75000,
+  },
+];
 
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    this.employeeService.getEmployees().subscribe({
+      next: (data) => this.employees = data,
+      error: (e) => console.error(e)
+    });
+  }
+
+  deleteEmployee(id: string) {
+    if(confirm('Are you sure?')) {
+      this.employeeService.deleteEmployee(id).subscribe(() => {
+        // Refresh list
+        this.loadEmployees();
+      });
+    }
+  }
 }
