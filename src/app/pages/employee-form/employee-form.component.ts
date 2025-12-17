22d6835db94f1791fadbed1348423e34412ba233
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-12-17 12:48:26 +0530

    Added Employee Form

--- src/app/pages/employee-form/employee-form.component.ts
+++ src/app/pages/employee-form/employee-form.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './employee-form.component.html',
-  styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  fb = inject(FormBuilder);
+  http = inject(EmployeeService);
+  router = inject(Router);
 
+  employeeForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    position: ['', Validators.required],
+    department: [''],
+    salary: [0]
+  });
+
+  onSubmit() {
+    if (this.employeeForm.valid) {
+      this.http.createEmployee(this.employeeForm.value as any).subscribe(() => {
+        this.router.navigate(['/']);
+      });
+    }
+  }
 }
